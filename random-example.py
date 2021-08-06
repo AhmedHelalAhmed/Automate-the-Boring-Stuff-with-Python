87828d5cf4f7754a7f4fe3e233747037c90a88d9
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-08-06 22:21:31 +0200

    Add random example

---
+++ random-example.py
@@ -0,0 +1,11 @@
+from random import randint
+
+print('Enter Min')
+min = int(input())
+print('Enter max')
+max = int(input())
+
+randomNumber = randint(min, max)
+
+print('The number is ')
+print(randomNumber)
